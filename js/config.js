5aaaf7a9e15e5b0deb8dab39c5bf01b52d8fc5e4
Enrique Soriano <jenriquesoriano@guadaltel.com>
2022-02-15 13:55:09 +0100

    staging

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var captchaEnabled = true;
+var betaBanner = false;
+var labelStaging = true;
+var environment = "STAGING";
+// PROD
+//var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+//var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
+//var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+//var serverToken = "";
+// SANDBOX
+// var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+// var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+// var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+//var serverToken = "";
+// STAGING
+var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
+var serverToken = "";
+
